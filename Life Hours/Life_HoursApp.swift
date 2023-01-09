eb7c197c09a105f956d989a9afd108bdfc47d008
Oncu <oohancan@gmail.com>
2023-01-09 08:25:39 +0300

    Initial Commit

---
+++ Life Hours/Life_HoursApp.swift
@@ -0,0 +1,17 @@
+//
+//  Life_HoursApp.swift
+//  Life Hours
+//
+//  Created by Oncu Can on 9.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct Life_HoursApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
