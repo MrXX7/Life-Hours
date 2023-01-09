d80cfa4fe45a2fe8080e70c30d90a8d55348c4cf
Oncu <oohancan@gmail.com>
2023-01-09 13:07:03 +0300

    add RegisterView

---
+++ Life Hours/RegisterView.swift
@@ -0,0 +1,36 @@
+//
+//  RegisterView.swift
+//  Life Hours
+//
+//  Created by Oncu Can on 9.01.2023.
+//
+
+import SwiftUI
+
+struct RegisterView: View {
+    @State private var email: String = ""
+    @State private var password: String = ""
+    
+    var body: some View {
+        Form {
+            Section {
+                TextField("Email", text: $email)
+                SecureField("Password", text: $password)
+            }
+            Button(action: register) {
+                Text("Register")
+            }
+        }
+    }
+    
+    func register() {
+        // perform registration logic here
+    }
+}
+
+
+struct RegisterView_Previews: PreviewProvider {
+    static var previews: some View {
+        RegisterView()
+    }
+}
